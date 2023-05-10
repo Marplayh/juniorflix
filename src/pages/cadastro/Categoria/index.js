2d8b2fd8f52c84c6491da0ff7cacb40a55039970
Marplayh <marciobrrj22@gmail.com>
2023-05-10 17:16:47 -0300

    Nova Atualização 2

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -5,12 +5,26 @@ import { Link } from "react-router-dom";
 function CadastroCategoria(){
     return (
         <PageDefault>
-            <h1>Página de Cadastro de Categoria</h1>
-            <Link to="/">
-                ir para home
-            </Link>
-        </PageDefault>
-    )
-}
+            <h1>Cadastro de Categoria</h1>
+
+                <form>
+                    <label>
+                        Nome da Categoria:
+                        <input
+                        type="text"
+                        />
+                    </label>
+                    <button>
+                        Cadastrar
+                    </button>
+                </form>
+
+
+                <Link to="/">
+                Ir para home
+                </Link>
+            </PageDefault>
+        )
+    }
 
 export default CadastroCategoria;
